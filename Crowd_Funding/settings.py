656e17bb9b719e3c718079a515c80643bdeb2411
AboAsil301 <aboasil9459@gmail.com>
2023-10-22 06:05:55 +0300

    Building the project structure & adding Bootstrap and fontawesome files

--- Crowd_Funding/settings.py
+++ Crowd_Funding/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ############################
+    'projects.apps.ProjectsConfig',
+    'user.apps.UserConfig',
+
 ]
 
 MIDDLEWARE = [
