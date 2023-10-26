de8d1f27ac78da14aac4cd298fa86fe1151e4cd1
AboAsil301 <aboasil9459@gmail.com>
2023-10-27 00:06:35 +0200

    Add and Costmize Registration And Login templates ,and Static Files  for them

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from user.views import *
 urlpatterns = [
   # path('',index, name='projects.index'),
     path('register/', register, name='register'),
+    path('login/', login, name='login'),
 ]
--- user/views.py
+++ user/views.py
@@ -12,4 +12,22 @@ def register(request):
     else:
         form = RegistrationForm()
 
-    return render(request, 'user/registration_form.html', {'form': form})
+    # return render(request, 'user/registration_form.html', {'form': form})
+    return render(request, 'user/registration.html', {'form': form})
+
+
+def login(request):
+    # if request.method == 'POST':
+    #     form = userLogin(request.POST)
+    #     if form.is_valid():
+    #         username = form.cleaned_data.get("username")
+    #         password = form.cleaned_data.get("password")
+    #         user = authenticate(username=username, password=password)
+    #         login(request, user)
+    #         user_info = User.objects.get(username=username)
+    #         # user_info2 = user_info.objects.Profile_set.all()
+    #         if user_info.is_active == True:
+    #             return redirect(index)
+    # else:
+    #     form = userLogin()
+    return render(request, "user/login.html")
