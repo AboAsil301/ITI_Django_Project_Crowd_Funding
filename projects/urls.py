82e6c07b1d816b78c512ddb6a21fd6568ba6d569
AboAsil301 <aboasil9459@gmail.com>
2023-10-29 23:48:56 +0200

    Update template and Adding viwe for a search Page

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
   path('',index, name='projects.index'),
   path('homepage', homepage, name='homepage'),
+  path('search', search, name='search'),
   path('projects/create/<int:user_id>', create_project, name='projects.create'),
   path('categories/create/', create_category, name='categories.create'),
   path('accounts/login/',login, name='login'),
